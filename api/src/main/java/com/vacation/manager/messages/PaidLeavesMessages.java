0ed36bb34670d2e794f80f66aae81fdbf601db9c
goloneczka <48791689+goloneczka@users.noreply.github.com>
2020-08-08 12:51:49 +0200

    check dates in new paidLeave (#24)

--- api/src/main/java/com/vacation/manager/messages/PaidLeavesMessages.java
+++ api/src/main/java/com/vacation/manager/messages/PaidLeavesMessages.java
@@ -3,5 +3,6 @@ package com.vacation.manager.messages;
 public class PaidLeavesMessages {
     public final static String NOT_FOUND = "Nie znaleziono takiego zgłoszenia";
     public final static String CREATE_FAILURE = "Nie udało się utworzyć zwolnienia";
+    public final static String FAILURE_EXCEPTION_DURATION = "Wybrany dzień jest użyty w innym zwolnieniu";
     public final static String UPDATE_FAILURE = "Nie udało się zaktualizować zwolnienia";
 }
